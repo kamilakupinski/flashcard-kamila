c5d336341e4ec7f73e7dc7498926417726b003e6
kamilakupinski <kupinski.kamila@escola.pr.gov.br>
2025-06-05 11:06:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,82 @@
+criaCartao(
+    'matemática',
+    'quanto é 9!?',
+    '362880'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'é uma linguagem de programação'
+)
+
+criaCartao(
+    'sociologia',
+    'o que são fatos sociais?',
+    'são modos de agir, pensar e sentir'
+)
+
+criaCartao(
+    'mat1',
+    'a que temperatura a água ferve?',
+    'a 100 graus'
+)
+
+criaCartao(
+    'inglês',
+    'como se fala "de nada" em inglês?"',
+    '"you´re welcome"'
+)
+
+criaCartao(
+    'biologia',
+    'quais animais fazem parte do filo dos cordados?',
+    'peixes, tubaroes, etc.'
+)
+criaCartao(
+    'robótica',
+    'em qual site podemos fazer um led brilhar?',
+    'arduino ide'
+)
+
+criaCartao(
+    'projeto de vida',
+    '',
+    ''
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
